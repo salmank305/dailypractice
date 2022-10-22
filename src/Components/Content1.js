6d5996930929685d2d5428a7e46628a2cfb0706a
salman305 <salmankhatik300591@gmail.com>
2022-10-22 22:53:10 +0530

    reactproject

---
+++ src/Components/Content1.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+/*
+ class Content1 extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>salman</h1>
+                <p>Random testing is a black-box software testing technique where programs are tested by generating random, independent inputs. Results of the output are compared against software specifications to verify that the test output is pass or fail</p>
+                <p>Random Blood Sugar Test. This measures your blood sugar at the time you're tested. You can take this test at any time and don't need to fast (not eat) first ...</p>
+            </div>
+        )
+    }
+}
+
+ class Content2 extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>salman</h1>
+                <p>Random testing is a black-box software testing technique where programs are tested by generating random, independent inputs. Results of the output are compared against software specifications to verify that the test output is pass or fail</p>
+                <p>Random testing is a black-box software testing technique where programs are tested by generating random, independent inputs. Results of the output are compared against software specifications to verify that the test output is pass or fail</p>
+            </div>
+        )
+    }
+}
+export{Content1,Content2};  //more than two class
+*/
+
+
+
+
+
+class Content1 extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>salman</h1>
+                <p>Random testing is a black-box software testing technique where programs are tested by generating random, inde inputs. Results of the output are compared against software specifications to verify that the test output is pass or fail</p>
+                <p>Random Blood Sugar Test. This measures your blood sugar at the time you're tested. You can take this test at any time and don't need to fast (not eat) first ...</p>
+            </div>
+        )
+    }
+}
+export default Content1;
---
+++ src/Components/Content2.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+class Content2 extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>khatik</h1>
+                <p> Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham</p>
+                <p>Random Blood Sugar Test. This measures your blood sugar at the time you're tested. You can take this test at any time and don't need to fast (not eat) first ...</p>
+            </div>
+        )
+    }
+}
+export default Content2;
